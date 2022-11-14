68e0deb8de98973929bb29aaa4cf533afd5d8e25
gershomlapaix <nsengiyumv33@gmail.com>
2022-11-15 00:09:54 +0200

    spring container configuration

--- IOCDemo/src/com/lapaix/ioc/AppDemo.java
+++ IOCDemo/src/com/lapaix/ioc/AppDemo.java
@@ -1,10 +1,27 @@
 package com.lapaix.ioc;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AppDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try {
+			// load spring configuration file 
+			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+			
+			// retrieve bean from the spring container
+			Coach theCoach = context.getBean("myCoach", Coach.class);
+			
+			// call methods on the bean
+			theCoach.getDailyWorkout();
+			
+			// close the context
+			context.close();
+		} catch (Exception e) {
+			// TODO: handle exception\
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
