a1a8c565d716c78a5313bfeef53a1627cc9625f5
gershomlapaix <nsengiyumv33@gmail.com>
2022-11-14 23:47:20 +0200

    first commit

---
+++ IOCDemo/src/com/lapaix/ioc/AppDemo.java
@@ -0,0 +1,10 @@
+package com.lapaix.ioc;
+
+public class AppDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ IOCDemo/src/com/lapaix/ioc/BaseballCoach.java
@@ -0,0 +1,11 @@
+package com.lapaix.ioc;
+
+public class BaseballCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Make five pushups each morning";
+	}
+
+}
