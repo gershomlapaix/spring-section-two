0368be566f4231841e33883089b6f34002683ad6
gershomlapaix <nsengiyumv33@gmail.com>
2022-12-01 00:14:55 +0200

    configure and use hibernate annotations

--- hibernateTutorial/src/com/lapaix/entity/Student.java
+++ hibernateTutorial/src/com/lapaix/entity/Student.java
@@ -1,13 +1,24 @@
 package com.lapaix.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="student")
 public class Student {
 	
-	String firstNaeme;
-	String lastName;
-	String email;
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
 }
