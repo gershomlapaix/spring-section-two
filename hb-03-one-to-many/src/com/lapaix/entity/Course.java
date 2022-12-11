6230eca56b850dc00df7c2cfd39efe98f73693cf
gershomlapaix <nsengiyumv33@gmail.com>
2022-12-11 18:19:42 +0200

    feat:define the cascade types

--- hb-03-one-to-many/src/com/lapaix/entity/Course.java
+++ hb-03-one-to-many/src/com/lapaix/entity/Course.java
@@ -1,6 +1,13 @@
 package com.lapaix.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -14,8 +21,16 @@ public class Course {
 	 *  annotate fields
 	 * */
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
+	
+	@Column(name="title")
 	private String title;
+
+	@ManyToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE, CascadeType.DETACH, CascadeType.MERGE})
+	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
 	
 	public Course() {}
