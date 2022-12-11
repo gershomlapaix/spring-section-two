0bf8cf0d15bcd32d3abe8baca1885382bb2442b9
gershomlapaix <nsengiyumv33@gmail.com>
2022-12-11 15:35:58 +0200

    feat: getting the instructor detail and the associated instructor:

---
+++ hb-one-to-one-bi/src/com/lapaix/hibernatedemo/GetInstructorDetailDemo.java
@@ -0,0 +1,50 @@
+package com.lapaix.hibernatedemo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.lapaix.entity.Instructor;
+import com.lapaix.entity.InstructorDetail;
+import com.lapaix.entity.Student;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		// create a session factory
+		SessionFactory sessionFactory = new Configuration()
+				.configure()
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		// create a session
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {
+			
+			
+			// start transaction
+			session.beginTransaction();
+			
+			// get the instructor detail object
+			int theId = 2;
+			InstructorDetail insDetail = session.get(InstructorDetail.class, theId);
+			
+			// print the instructor detail
+			System.out.println("Instructor details" +insDetail);
+			
+			// get the associated instructor
+			 System.out.println("Associated instructor: "+ insDetail.getInstructor());
+			
+			// commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ hb-one-to-one-bi/src/com/lapaix/jdbc/TestJdbc.java
@@ -0,0 +1,29 @@
+package com.lapaix.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+		
+		// useSSL: avoid mysql ssl warnings
+		public static void main(String[] args) {
+
+			String jdbcUrl = "jdbc:mysql://localhost:3306/one_to_one_uni?useSSL=false";
+			String user = "hbstudent";
+			String pass = "hbstudent";
+			
+			try {
+				System.out.println("Connecting to database: " + jdbcUrl);
+				
+				Connection myConn =
+						DriverManager.getConnection(jdbcUrl, user, pass);
+				
+				System.out.println("Connection successful!!!");
+				
+			}
+			catch (Exception exc) {
+				exc.printStackTrace();
+			}
+	}
+
+}
